f947553b38aee83beb5ca618fbfad6dcbf17252e
farzaadshojaei <farzaadshojaei@gmail.com>
2023-10-26 19:54:54 +0330

    Adding Test Order

---
+++ test/com/appsdeveloperblog/MethodOrderedRandomlyTest.java
@@ -0,0 +1,29 @@
+package com.appsdeveloperblog;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.Random.class)
+public class MethodOrderedRandomlyTest {
+
+    @Test
+    void testA(){
+        System.out.println("Running Test A");
+    }
+
+    @Test
+    void TestB(){
+        System.out.println("Running Test B");
+    }
+
+    @Test
+    void TestC(){
+        System.out.println("Running Test C");
+    }
+
+    @Test
+    void TestD(){
+        System.out.println("Running Test D");
+    }
+}
